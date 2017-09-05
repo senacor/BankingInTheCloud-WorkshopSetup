c52cd48610102461731e546dbcdd6fa6cbfab386
Daniel Karzel <daniel.karzel@senacor.com>
2017-09-05 18:53:00 +0200

    added a backup branch that contains tool backup

--- during-workshop/BankingInTheCloud-VM/bootstrap.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# not needed any more, because the repository has to be forked
-#git clone https://github.com/senacor/BankingInTheCloud-Fineract.git
-
-# give rights to vagrant on the folder
-#sudo chown -R vagrant:vagrant BankingInTheCloud-Fineract/
